23bf97c8cbcae4ab5e82b2a51034cfc692835898
avgoeid <avgoeid@gmail.com>
2021-11-11 22:33:04 +0200

    GC-813: add LOG_FILE_PATH

--- logger/logger.go
+++ logger/logger.go
@@ -1,8 +1,10 @@
 package logger
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
@@ -22,6 +24,8 @@ func init() {
 		instance = "anonymous raccoon"
 	}
 
+	Logger.logFilePath = os.Getenv("LOG_FILE_PATH")
+
 	logger := logrus.New()
 	lvl, err := logrus.ParseLevel(level)
 	if err != nil {
@@ -37,69 +41,101 @@ func init() {
 
 	logWriters := []io.Writer{os.Stdout}
 
-	Logger.logFile, err = prepLogFile("cpaasFileTransfer", "fts.log")
-	if err != nil {
-		logrus.Fatalf("prepare logfile: %s", err)
-	} else {
-		logWriters = append(logWriters, Logger.logFile)
+	if Logger.isWriteToFile() {
+		err = Logger.prepLogFile()
+		if err != nil {
+			logrus.Fatalf("prepare logfile: %s", err)
+		} else {
+			logWriters = append(logWriters, Logger.logFile)
+		}
 	}
 
 	logger.SetOutput(io.MultiWriter(logWriters...))
 }
 
 type FtLogger struct {
-	Logger  *logrus.Entry
-	logFile *os.File
+	Logger      *logrus.Entry
+	logFile     *os.File
+	logFilePath string
 }
 
-func (ft *FtLogger) Close() {
-	ft.logFile.Close()
+func (l *FtLogger) isWriteToFile() bool {
+	return len(l.logFilePath) > 0
 }
 
-func (ft *FtLogger) Debug(args ...interface{}) {
-	ft.Logger.Debug(args...)
+func (l *FtLogger) prepLogFile() error {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return fmt.Errorf("get user cache dir: %s", err)
+	}
+
+	logFileDir := filepath.Dir(l.logFilePath)
+	logFileDirPath := filepath.Join(cacheDir, logFileDir)
+	err = os.MkdirAll(logFileDirPath, 0755)
+	if err != nil {
+		return fmt.Errorf("make \"%s\" dir: %s", logFileDirPath, err)
+	}
+
+	logFilePath := filepath.Join(cacheDir, l.logFilePath)
+
+	l.logFile, err = os.OpenFile(logFilePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (ft *FtLogger) Debugf(format string, args ...interface{}) {
-	ft.Logger.Debugf(format, args...)
+func (l *FtLogger) Close() {
+	if l.isWriteToFile() {
+		l.logFile.Close()
+	}
 }
 
-func (ft *FtLogger) Info(args ...interface{}) {
-	ft.Logger.Info(args...)
+func (l *FtLogger) Debug(args ...interface{}) {
+	l.Logger.Debug(args...)
 }
 
-func (ft *FtLogger) Infof(format string, args ...interface{}) {
-	ft.Logger.Infof(format, args...)
+func (l *FtLogger) Debugf(format string, args ...interface{}) {
+	l.Logger.Debugf(format, args...)
 }
 
-func (ft *FtLogger) Warning(args ...interface{}) {
-	ft.Logger.Warning(args...)
+func (l *FtLogger) Info(args ...interface{}) {
+	l.Logger.Info(args...)
 }
 
-func (ft *FtLogger) Warningf(format string, args ...interface{}) {
-	ft.Logger.Warningf(format, args...)
+func (l *FtLogger) Infof(format string, args ...interface{}) {
+	l.Logger.Infof(format, args...)
 }
 
-func (ft *FtLogger) Error(args ...interface{}) {
-	ft.Logger.Error(args...)
+func (l *FtLogger) Warning(args ...interface{}) {
+	l.Logger.Warning(args...)
 }
 
-func (ft *FtLogger) Errorf(format string, args ...interface{}) {
-	ft.Logger.Errorf(format, args...)
+func (l *FtLogger) Warningf(format string, args ...interface{}) {
+	l.Logger.Warningf(format, args...)
 }
 
-func (ft *FtLogger) Fatal(args ...interface{}) {
-	ft.Logger.Fatal(args...)
+func (l *FtLogger) Error(args ...interface{}) {
+	l.Logger.Error(args...)
 }
 
-func (ft *FtLogger) Fatalf(format string, args ...interface{}) {
-	ft.Logger.Fatalf(format, args...)
+func (l *FtLogger) Errorf(format string, args ...interface{}) {
+	l.Logger.Errorf(format, args...)
 }
 
-func (ft *FtLogger) Print(args ...interface{}) {
-	ft.Logger.Print(args...)
+func (l *FtLogger) Fatal(args ...interface{}) {
+	l.Logger.Fatal(args...)
 }
 
-func (ft *FtLogger) Printf(format string, args ...interface{}) {
-	ft.Logger.Printf(format, args...)
+func (l *FtLogger) Fatalf(format string, args ...interface{}) {
+	l.Logger.Fatalf(format, args...)
+}
+
+func (l *FtLogger) Print(args ...interface{}) {
+	l.Logger.Print(args...)
+}
+
+func (l *FtLogger) Printf(format string, args ...interface{}) {
+	l.Logger.Printf(format, args...)
 }
