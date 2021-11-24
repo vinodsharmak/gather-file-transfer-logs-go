aa6d79e8e3039ac501975c88f579edaedcbd5a00
Nehal Shaquib <nehal@gather.network>
2021-11-24 13:59:30 +0530

    GC-813: Updated collecting error responses

--- logger/logger.go
+++ logger/logger.go
@@ -154,16 +154,23 @@ func (l *FtLogger) Printf(format string, args ...interface{}) {
 	l.Logger.Printf(format, args...)
 }
 
+type response struct {
+	Details   string `json:"details"`
+	Instance  string `json:"instance"`
+	MachineID string `json:"machine_id"`
+	Detail    string `json:"detail"`
+	Error     string `json:"error"`
+}
+
 func (l *FtLogger) sendLogs(auth string, machinePairID string) error {
 	cacheDir, err := os.UserCacheDir()
 	if err != nil {
-		Logger.Fatalf("get user cache dir: %s", err)
-		return err
+		return fmt.Errorf("get user cache dir: %v", err)
 	}
 	logFilePath := filepath.Join(cacheDir, l.logFilePath)
 	content, err := ioutil.ReadFile(logFilePath)
 	if err != nil {
-		Logger.Fatalf("error in reading log file: ", err)
+		return fmt.Errorf("error in reading log file: %v", err)
 	}
 	machineID := ""
 	if instance == "sender" || instance == "receiver" {
@@ -176,13 +183,11 @@ func (l *FtLogger) sendLogs(auth string, machinePairID string) error {
 
 	requestBody, err := json.Marshal(l)
 	if err != nil {
-		Logger.Errorf("request body: ", err)
-		return err
+		return fmt.Errorf("request body: %v", err)
 	}
 	req, err := http.NewRequest("POST", Logger.controllerURL+"/api/v1/file_transfer/machine_pair/"+machinePairID+"/logs/", bytes.NewBuffer(requestBody))
 	if err != nil {
-		Logger.Errorf("creating request: ", err)
-		return err
+		return fmt.Errorf("creating request: %v", err)
 	}
 
 	req.Header.Add("Authorization-Token", auth)
@@ -191,52 +196,36 @@ func (l *FtLogger) sendLogs(auth string, machinePairID string) error {
 	client := http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		Logger.Errorf("sending request:", err)
-		return err
+		return fmt.Errorf("sending request: %v", err)
 	}
 	defer resp.Body.Close()
 
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	var data response
+	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
-		Logger.Errorf("unexpected error reading response:", err)
-		return err
-	}
-	var data map[string]interface{}
-	err = json.Unmarshal([]byte(bodyBytes), &data)
-	if err != nil {
-		Logger.Errorf("unexpected error in unmarshal:", err)
-		return err
+		return fmt.Errorf("response: %v", err)
 	}
 
 	switch resp.StatusCode {
 	case http.StatusOK:
-		Logger.Infof("Succes response: ", data["details"])
+		Logger.Infof("Succes response: ", data.Details)
 		return nil
 
 	case http.StatusBadRequest:
-		if msg, ok := data["instance"]; ok {
-			Logger.Errorf("Failed: ", msg)
-			return fmt.Errorf("%v", msg)
+		if data.Instance != "" {
+			return fmt.Errorf("error-400: %v", data.Instance)
 		}
-		if msg, ok := data["machine_id"]; ok {
-			Logger.Errorf("Failed: ", msg)
-			return fmt.Errorf("%v", msg)
+		if data.MachineID != "" {
+			return fmt.Errorf("error-400: %v", data.MachineID)
 		}
 		return fmt.Errorf("Error: 400")
 
 	case http.StatusNotFound:
-		if msg, ok := data["detail"]; ok {
-			Logger.Errorf("Failed: ", msg)
-			return fmt.Errorf("%v", msg)
-		}
-		return fmt.Errorf("Error: 404")
+		return fmt.Errorf("error-404: %v", data.Detail)
 
 	case http.StatusUnauthorized:
-		if msg, ok := data["error"]; ok {
-			Logger.Errorf("Failed: ", msg)
-			return fmt.Errorf("%v", msg)
-		}
-		return fmt.Errorf("Error: 401")
+		return fmt.Errorf("error-401: %v", data.Error)
+
 	}
 	return fmt.Errorf("Error Response: %v", resp.StatusCode)
 }
